c9856bcae444ec8eca454423231fa3ead6b3949a
Davy <davy.navy.3@gmail.com>
2021-03-09 21:42:22 +0100

    v1.0
    + Changed text box design.
    + Added 5 buttons.

--- iOcjene/iOcjene/MainPage.xaml.cs
+++ iOcjene/iOcjene/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace iOcjene
         {
             this.InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
